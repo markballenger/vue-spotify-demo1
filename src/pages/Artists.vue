cf74038503b1713c9250ffbfdc892345b525b5fd
mark ballenger <mark.ballenger@e-hps.com>
2017-10-31 13:35:25 +0000

    added build, Artists.vue

--- src/pages/Artists.vue
+++ src/pages/Artists.vue
@@ -1,8 +1,28 @@
 <template>
 <div>
-  <div v-for="a in artists" :key="a.name">
-   {{ a.name }}
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm12 >
+
+      <v-container fluid grid-list-sm>
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex xs2 v-for="a in artists" :key="a.name">
+              <v-card>
+                <v-card-media>
+                  <img class="image" :src="a.images[1].url"  :width="100" :height="100">
+                </v-card-media>
+                <v-card-actions>
+                  <v-btn icon>
+                    <v-icon>home</v-icon></v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        
+      </v-container>
+    </v-flex>
+  </v-layout>
  </div>
 </template>
 
